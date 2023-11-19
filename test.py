fb2b95c4ad6fb18fb94174b15887213dddb6c1dd
Gusers Missin <missingusers@gmail.com>
2023-11-20 06:28:29 +0900

    print some type of print

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print('hello')
+print('hello' + ' world')
+
+# print('hello)
+# SyntaxError: EOL while scanning string literal
+
+# print('hello' - ' World')
+# Exception has occurred: TypeError
+
+# print('hello' + 2)
+# TypeError: unsupported operand type(s) for -: 'str' and 'str'
+
+print('hello' + '2')
+
+print('hello' * 3)
